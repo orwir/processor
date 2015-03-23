c6aeb371625a444344eb48e9ae8ce8418b47585c
Igor Zubenko <igor.a.zubenko@gmail.com>
2015-03-23 22:51:50 +0300

    added clearing data from observer bundle.

--- processor/src/main/java/ingvar/android/processor/observation/ObserverManager.java
+++ processor/src/main/java/ingvar/android/processor/observation/ObserverManager.java
@@ -161,6 +161,7 @@ public class ObserverManager implements IObserverManager {
                     remove(request);
                 }
             }
+            bundle.clear();
 
             return true;
         }
